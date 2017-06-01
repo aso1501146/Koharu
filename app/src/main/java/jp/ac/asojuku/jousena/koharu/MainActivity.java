1aa2b239bb839c78c60d164c404d9c5e18785efd
Fill <1501146@st.asojuku.ac.jp>
2017-06-01 10:14:47 +0900

    com

--- app/src/main/java/jp/ac/asojuku/jousena/koharu/MainActivity.java
+++ app/src/main/java/jp/ac/asojuku/jousena/koharu/MainActivity.java
@@ -17,4 +17,5 @@ public class MainActivity extends AppCompatActivity {
     public void onCreateSupportNavigateUpTaskStack(@NonNull TaskStackBuilder builder) {
         super.onCreateSupportNavigateUpTaskStack(builder);
     }
+    
 }
